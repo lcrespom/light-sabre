77a9825313d978e6e99fe31b99a346e746be12cf
Luis Crespo <lcrespom@gmail.com>
2016-01-31 16:51:03 +0100

    More sounds

--- ls.js
+++ ls.js
@@ -1,6 +1,6 @@
 $(function() {
 	var ac = setupWA();
-	var buf_ls_on, buf_ls_off, buf_ls_swing;
+	var buf_ls_on, buf_ls_off, buf_ls_swing, buf_ls_hum;
 
 	$('#ls_on').click(_ => {
 		playBuffer(buf_ls_on);
@@ -11,11 +11,19 @@ $(function() {
 	$('#ls_swing').click(_ => {
 		playBuffer(buf_ls_swing);
 	});
+	$('#ls_hum').click(_ => {
+		playBuffer(buf_ls_hum, true);
+	});
 
-	function playBuffer(buf) {
+	function playBuffer(buf, loop) {
 		if (!buf) return;
 		var bufSrc = ac.createBufferSource();
 		bufSrc.buffer = buf;
+		if (loop) {
+			bufSrc.loop = true;
+			bufSrc.loopStart = 0;
+			bufSrc.loopEnd = 9999999;
+		}
 		bufSrc.connect(ac.destination);
 		bufSrc.start();
 	}
@@ -25,6 +33,7 @@ $(function() {
 		loadBuffer(ac, 'audio/saber-opening-short.wav', buffer => buf_ls_on = buffer);
 		loadBuffer(ac, 'audio/close.m4a', buffer => buf_ls_off = buffer);
 		loadBuffer(ac, 'audio/Light Saber Sounds-2-a.wav', buffer => buf_ls_swing = buffer);
+		loadBuffer(ac, 'audio/humm.mp3', buffer => buf_ls_hum = buffer);
 		return ac;
 	}
 
