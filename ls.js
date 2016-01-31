cc692339cdbe7237a0e4dba3eebc14f608ddaa6d
Luis Crespo <lcrespom@gmail.com>
2016-01-31 17:09:04 +0100

    Working in Safari

--- ls.js
+++ ls.js
@@ -2,16 +2,16 @@ $(function() {
 	var ac = setupWA();
 	var buf_ls_on, buf_ls_off, buf_ls_swing, buf_ls_hum;
 
-	$('#ls_on').click(_ => {
+	$('#ls_on').click(function() {
 		playBuffer(buf_ls_on);
 	});
-	$('#ls_off').click(_ => {
+	$('#ls_off').click(function() {
 		playBuffer(buf_ls_off);
 	});
-	$('#ls_swing').click(_ => {
+	$('#ls_swing').click(function() {
 		playBuffer(buf_ls_swing);
 	});
-	$('#ls_hum').click(_ => {
+	$('#ls_hum').click(function() {
 		playBuffer(buf_ls_hum, true);
 	});
 
@@ -29,11 +29,13 @@ $(function() {
 	}
 
 	function setupWA() {
-		var ac = new AudioContext();
-		loadBuffer(ac, 'audio/saber-opening-short.wav', buffer => buf_ls_on = buffer);
-		loadBuffer(ac, 'audio/close.m4a', buffer => buf_ls_off = buffer);
-		loadBuffer(ac, 'audio/Light Saber Sounds-2-a.wav', buffer => buf_ls_swing = buffer);
-		loadBuffer(ac, 'audio/humm.mp3', buffer => buf_ls_hum = buffer);
+		var ac;
+		if (window.AudioContext) ac = new window.AudioContext();
+		else ac = new window.webkitAudioContext();
+		loadBuffer(ac, 'audio/saber-opening-short.wav', function(buffer) { buf_ls_on = buffer });
+		loadBuffer(ac, 'audio/close.m4a', function(buffer) { buf_ls_off = buffer });
+		loadBuffer(ac, 'audio/Light Saber Sounds-2-a.wav', function(buffer) { buf_ls_swing = buffer });
+		loadBuffer(ac, 'audio/humm.mp3', function(buffer) { buf_ls_hum = buffer });
 		return ac;
 	}
 
@@ -41,8 +43,8 @@ $(function() {
 		const xhr = new XMLHttpRequest();
 		xhr.open('GET', url, true);
 		xhr.responseType = 'arraybuffer';
-		xhr.onload = _ => {
-			ac.decodeAudioData(xhr.response, buffer => {
+		xhr.onload = function() {
+			ac.decodeAudioData(xhr.response, function(buffer) {
 				cb(buffer);
 			});
 		};
