cd5c36720b49184ad960b41d5a2f22f6a34c9267
Luis Crespo <lcrespom@gmail.com>
2016-01-31 16:12:15 +0100

    Update

--- ls.js
+++ ls.js
@@ -1,1 +1,35 @@
-console.log('Hello world');
+$(function() {
+	var ac = setupWA();
+	var buf_ls_on;
+
+	$('#sound1').click(_ => {
+		playBuffer(buf_ls_on);
+	});
+
+	function playBuffer(buf) {
+		if (!buf) return;
+		var bufSrc = ac.createBufferSource();
+		bufSrc.buffer = buf;
+		bufSrc.connect(ac.destination);
+		bufSrc.start();
+	}
+
+	function setupWA() {
+		var ac = new AudioContext();
+		loadBuffer(ac, 'audio/saber-opening-short.wav', buffer => buf_ls_on = buffer);
+		return ac;
+	}
+
+	function loadBuffer(ac, url, cb) {
+		const xhr = new XMLHttpRequest();
+		xhr.open('GET', url, true);
+		xhr.responseType = 'arraybuffer';
+		xhr.onload = _ => {
+			ac.decodeAudioData(xhr.response, buffer => {
+				cb(buffer);
+			});
+		};
+		xhr.send();
+	}
+
+});
